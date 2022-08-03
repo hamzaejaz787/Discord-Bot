ba7ebab9cbff2a951161627e1b2751984576a857
hamzaejaz787 <hejaz389@gmail.com>
2022-08-03 21:28:03 +0500

    Bot updates

--- index.js
+++ index.js
@@ -1,64 +1,87 @@
-// Require the necessary discord.js classes
-const { Client, Intents, Message, MessageEmbed } = require("discord.js");
-const dotenv = require("dotenv");
-dotenv.config();
+import { config } from "dotenv";
+import { Client, GatewayIntentBits, Routes } from "discord.js";
+import { REST } from "@discordjs/rest";
 
-// Create a new client instance
+config();
+
+const TOKEN = process.env.DISCORD_BOT_TOKEN;
+const CLIENT_ID = process.env.CLIENT_ID;
+const GUILD_ID = process.env.GUILD_ID;
+
 const client = new Client({
-  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+const rest = new REST({ version: "10" }).setToken(TOKEN);
+
+client.on("ready", () => console.log(`${client.user.tag} has logged in...`));
+
+client.on("interactionCreate", (interaction) => {
+  if (interaction.isChatInputCommand()) {
+    interaction.reply({
+      content: `You have ordered a ${
+        interaction.options.get("food").value
+      } and a ${interaction.options.get("drink").value}`,
+    });
+  }
 });
 
-// When the client is ready, run this code
-client.on("ready", () => {
-  console.log(`${client.user.tag} has logged in!`);
-});
+// client.on("messageCreate", (message) => {
+//   console.log(`${message.author.tag}: ${message.content}`);
+// });
 
-client.on("messageCreate", (message) => {
-  if (message.author.bot) {
-    return;
-  } else if (message.content.toLowerCase() === "hello") {
-    message.reply("Hello");
+(async function main() {
+  const commands = [
+    {
+      name: "order",
+      description: "Order something...",
+      options: [
+        {
+          name: "food",
+          description: "type of food",
+          type: 3,
+          required: true,
+          choices: [
+            { name: "Burger", value: "burger" },
+            { name: "Pizza", value: "pizza" },
+          ],
+        },
+        {
+          name: "drink",
+          description: "type of drink",
+          type: 3,
+          required: false,
+          choices: [
+            { name: "Water", value: "water" },
+            { name: "Coke", value: "coke" },
+            { name: "Sprite", value: "sprite" },
+          ],
+        },
+        {
+          name: "dessert",
+          description: "type of dessert",
+          type: 3,
+          required: false,
+          choices: [
+            { name: "Strawberry Cake", value: "strawberry cake" },
+            { name: "Chocolate Cake", value: "chocolate cake" },
+          ],
+        },
+      ],
+    },
+  ];
+
+  try {
+    console.log("Started refreshing application (/) commands.");
+    await rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), {
+      body: commands,
+    });
+
+    client.login(TOKEN);
+  } catch (err) {
+    console.log(err);
   }
-
-  if (message.content == "$listCommands") {
-    const exampleEmbed = new MessageEmbed()
-      .setColor("#ffd046")
-      .setTitle("Server Commands")
-      .setDescription(
-        "Here you can see the list of the commands used on the server: "
-      )
-      .addFields(
-        { name: "`$like`", value: "Likes the current message" },
-        { name: "`$dislike`", value: "Dislikes the current message" },
-        { name: "`$random`", value: "Returns a random number" }
-      );
-    message.channel.send({ embeds: [exampleEmbed] });
-  }
-
-  if (message.content === "$like") {
-    message.react("👍");
-  }
-
-  if (message.content === "$dislike") {
-    message.react("👎");
-  }
-
-  if (message.content === "$random") {
-    let randomNumber = getRandomNumber(0, 1000);
-    message.reply(`Here's a random number for you ${randomNumber}`);
-  }
-
-  function getRandomNumber(min, max) {
-    return Math.floor(Math.random() * (max - min) + min);
-  }
-
-  console.log(`[${message.author.tag}]: ${message.content}`);
-});
-
-// Login to Discord with your client's token
-client.login(process.env.DISCORD_TOKEN).then(() => {
-  client.user.setPresence({
-    activities: [{ name: "music", type: "LISTENING" }],
-    status: "online",
-  });
-});
+})();
